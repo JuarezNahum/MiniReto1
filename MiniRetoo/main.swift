0990a35023d5c6f6d8d3cf90e8de40d3d3b30bf6
Nahúm Júarez <NahumJuarez@mbp-de-nahum.lan>
2015-11-12 23:18:23 -0600

    Initial Commit

---
+++ MiniRetoo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MiniRetoo
+//
+//  Created by Nahúm Júarez on 12/11/15.
+//  Copyright © 2015 Nahúm Júarez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
